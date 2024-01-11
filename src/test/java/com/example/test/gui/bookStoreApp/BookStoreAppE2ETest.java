4eaf749dbe70d5c1f04ec3f27bbcc36f663a71c4
dj-luis198 <dj.luis198@gmail.com>
2024-01-11 08:48:11 -0300

    commit

--- src/test/java/com/example/test/gui/bookStoreApp/BookStoreAppE2ETest.java
+++ src/test/java/com/example/test/gui/bookStoreApp/BookStoreAppE2ETest.java
@@ -31,7 +31,7 @@ public class BookStoreAppE2ETest extends BaseTest {
         this.books = books;
     }
 
-    @Test
+    @Test(timeOut = 90000)
     public void bookStoreE2E() {
         ProfilePage profilePage = loginPage.login(username, password);
         Assert.assertEquals(profilePage.getUserName(), username);
